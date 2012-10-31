7cc1e83f41b32421c3c9463ae3c0cfe326fb1640
Frederick Cheung <frederick.cheung@gmail.com>
2012-10-31 08:41:42 +0000

    added cf date

---
+++ lib/keychain/cf/date.rb
@@ -0,0 +1,18 @@
+module CF
+  typedef :pointer, :cfdateref
+  typedef :double, :cfabsolutetime
+
+  attach_function :CFDateCreate, [:pointer, :cfabsolutetime], :cfdateref
+  attach_function :CFDateGetAbsoluteTime, [:cfdateref], :cfabsolutetime
+  attach_variable :kCFAbsoluteTimeIntervalSince1970, :double
+
+  class Date < Base
+    def self.from_time(time)
+      new(CF.CFDateCreate(nil, time.to_f - CF.kCFAbsoluteTimeIntervalSince1970,)).release_on_gc
+    end
+
+    def to_time
+      Time.at(CF.CFDateGetAbsoluteTime(self) + CF.kCFAbsoluteTimeIntervalSince1970)
+    end
+  end
+end
--- spec/cf_spec.rb
+++ spec/cf_spec.rb
@@ -191,6 +191,24 @@ describe CF do
     end
   end
 
+  describe CF::Date do
+    describe('from_time') do
+      it 'should create a cf date from a time' do
+        CF::Date.from_time(Time.now).should be_a(CF::Date)
+      end
+    end
+
+    describe('to_time') do
+      it 'should return a time' do
+        t = CF::Date.from_time(Time.now).to_time
+        t.should be_a(Time)
+        t.should be_within(0.001).of(Time.now)
+      end
+    end
+
+
+  end
+
 
   describe CF::Number do
 
