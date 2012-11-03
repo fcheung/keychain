f8fe97ab0b4302603b9a485fb3bc228c76987eea
Frederick Cheung <frederick.cheung@gmail.com>
2012-11-03 15:08:26 +0000

    fix typo in method name

--- lib/keychain/protocols.rb
+++ lib/keychain/protocols.rb
@@ -2,42 +2,42 @@
 # Procotol constants for use when creating/finding a Keychain::Item of type internet password
 #
 #
-#
 module Keychain::Protocols
-  def self.attach_protocal name
+  # @private
+  def self.attach_protocol name
     Sec.send :attach_variable, name, :pointer
     constant_name = name.to_s.gsub('kSecAttrProtocol', '').upcase
     const_set constant_name, CF::Base.typecast(Sec.send(name))
   end
-  attach_protocal :kSecAttrProtocolFTP
-  attach_protocal :kSecAttrProtocolFTPAccount
-  attach_protocal :kSecAttrProtocolHTTP
-  attach_protocal :kSecAttrProtocolIRC
-  attach_protocal :kSecAttrProtocolNNTP
-  attach_protocal :kSecAttrProtocolPOP3
-  attach_protocal :kSecAttrProtocolSMTP
-  attach_protocal :kSecAttrProtocolSOCKS
-  attach_protocal :kSecAttrProtocolIMAP
-  attach_protocal :kSecAttrProtocolLDAP
-  attach_protocal :kSecAttrProtocolAppleTalk
-  attach_protocal :kSecAttrProtocolAFP
-  attach_protocal :kSecAttrProtocolTelnet
-  attach_protocal :kSecAttrProtocolSSH
-  attach_protocal :kSecAttrProtocolFTPS
-  attach_protocal :kSecAttrProtocolHTTPS
-  attach_protocal :kSecAttrProtocolHTTPProxy
-  attach_protocal :kSecAttrProtocolHTTPSProxy
-  attach_protocal :kSecAttrProtocolFTPProxy
-  attach_protocal :kSecAttrProtocolSMB
-  attach_protocal :kSecAttrProtocolRTSP
-  attach_protocal :kSecAttrProtocolRTSPProxy
-  attach_protocal :kSecAttrProtocolDAAP
-  attach_protocal :kSecAttrProtocolEPPC
-  attach_protocal :kSecAttrProtocolIPP
-  attach_protocal :kSecAttrProtocolNNTPS
-  attach_protocal :kSecAttrProtocolLDAPS
-  attach_protocal :kSecAttrProtocolTelnetS
-  attach_protocal :kSecAttrProtocolIMAPS
-  attach_protocal :kSecAttrProtocolIRCS
-  attach_protocal :kSecAttrProtocolPOP3S
+  attach_protocol :kSecAttrProtocolFTP
+  attach_protocol :kSecAttrProtocolFTPAccount
+  attach_protocol :kSecAttrProtocolHTTP
+  attach_protocol :kSecAttrProtocolIRC
+  attach_protocol :kSecAttrProtocolNNTP
+  attach_protocol :kSecAttrProtocolPOP3
+  attach_protocol :kSecAttrProtocolSMTP
+  attach_protocol :kSecAttrProtocolSOCKS
+  attach_protocol :kSecAttrProtocolIMAP
+  attach_protocol :kSecAttrProtocolLDAP
+  attach_protocol :kSecAttrProtocolAppleTalk
+  attach_protocol :kSecAttrProtocolAFP
+  attach_protocol :kSecAttrProtocolTelnet
+  attach_protocol :kSecAttrProtocolSSH
+  attach_protocol :kSecAttrProtocolFTPS
+  attach_protocol :kSecAttrProtocolHTTPS
+  attach_protocol :kSecAttrProtocolHTTPProxy
+  attach_protocol :kSecAttrProtocolHTTPSProxy
+  attach_protocol :kSecAttrProtocolFTPProxy
+  attach_protocol :kSecAttrProtocolSMB
+  attach_protocol :kSecAttrProtocolRTSP
+  attach_protocol :kSecAttrProtocolRTSPProxy
+  attach_protocol :kSecAttrProtocolDAAP
+  attach_protocol :kSecAttrProtocolEPPC
+  attach_protocol :kSecAttrProtocolIPP
+  attach_protocol :kSecAttrProtocolNNTPS
+  attach_protocol :kSecAttrProtocolLDAPS
+  attach_protocol :kSecAttrProtocolTelnetS
+  attach_protocol :kSecAttrProtocolIMAPS
+  attach_protocol :kSecAttrProtocolIRCS
+  attach_protocol :kSecAttrProtocolPOP3S
 end
