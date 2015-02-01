272e27627239f29fce3e2918b1ff4efa73b33ffc
Frederick Cheung <frederick.cheung@gmail.com>
2015-02-01 20:44:41 +0000

    make identity specs use a keychain file bundled with the specs
    don't rely on what might be in the default keychain

--- spec/identity_spec.rb
+++ spec/identity_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe Keychain::Identity do
+  before(:context) do
+    @keychain = Keychain.open(File.join(File.dirname(__FILE__), 'spec.keychain'))
+    @keychain.unlock! 'DummyPassword'
+  end
+
   describe 'query' do
     it 'should return a identity' do
-      scope = Keychain::Scope.new(Sec::Classes::IDENTITY)
+      scope = Keychain::Scope.new(Sec::Classes::IDENTITY, @keychain)
       identities = scope.all
       expect(identities.length).to be > 0
       expect(identities.first).to be_kind_of(Keychain::Identity)
@@ -12,21 +17,21 @@ describe Keychain::Identity do
 
   describe 'identify' do
     it 'should have a certificate' do
-      scope = Keychain::Scope.new(Sec::Classes::IDENTITY)
+      scope = Keychain::Scope.new(Sec::Classes::IDENTITY, @keychain)
       identity = scope.all.first
       expect(identity.certificate).to be_kind_of(Keychain::Certificate)
     end
 
     it 'should have a private key' do
-      scope = Keychain::Scope.new(Sec::Classes::IDENTITY)
+      scope = Keychain::Scope.new(Sec::Classes::IDENTITY, @keychain)
       identity = scope.all.first
       expect(identity.private_key).to be_kind_of(Keychain::Key)
     end
 
     it 'should be exportable to pkcs12' do
-      scope = Keychain::Scope.new(Sec::Classes::IDENTITY)
+      scope = Keychain::Scope.new(Sec::Classes::IDENTITY, @keychain)
       identity = scope.all.first
-      expect(identity.pkcs12).to be_kind_of(OpenSSL::PKCS12)
+      expect(identity.pkcs12()).to be_kind_of(OpenSSL::PKCS12)
     end
   end
 end
